f50e723790b5f396d97ec66b6a9f76a8d01d6138
eduard-ops <epustynnik@gmail.com>
2022-05-22 13:53:05 +0300

    App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import Container from './Conatainer';
+
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+import SearchBar from './Searchbar';
+
+import ImageGallery from './ImageGallery';
+
+class App extends Component {
+  state = {
+    imageName: '',
+  };
+
+  hundleFormSubmit = imageName => {
+    this.setState({ imageName: imageName });
+  };
+
+  render() {
+    const { imageName } = this.state;
+    return (
+      <Container>
+        <SearchBar onSubmit={this.hundleFormSubmit} />
+        <ToastContainer autoClose={3000} pauseOnHover={false} />
+        <ImageGallery imageName={imageName} />
+      </Container>
+    );
+  }
+}
+
+export { App };
