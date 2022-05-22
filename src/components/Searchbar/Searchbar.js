5f00b0788dc28f8b210524e3ba9df81541fa99ab
eduard-ops <epustynnik@gmail.com>
2022-05-22 13:54:54 +0300

    Searchbar.js

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,49 @@
+import { ReactComponent as IconSearch } from '../../images/icon-search.svg';
+
+import { Component } from 'react';
+
+import { toast } from 'react-toastify';
+
+class SearchBar extends Component {
+  state = {
+    nameImage: '',
+  };
+
+  handleNameChange = e => {
+    this.setState({ nameImage: e.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = e => {
+    const { nameImage } = this.state;
+    e.preventDefault();
+    if (nameImage.trim() === '') {
+      toast.error('введите имя', { theme: 'colored' });
+      return;
+    }
+    this.props.onSubmit(nameImage);
+    this.setState({ nameImage: '' });
+  };
+
+  render() {
+    return (
+      <header className="header">
+        <form onSubmit={this.handleSubmit} className="header-form">
+          <button type="submit" className="header-form__button-search">
+            <IconSearch />
+          </button>
+          <input
+            className="header-form__input-search"
+            type="text"
+            value={this.state.nameImage}
+            onChange={this.handleNameChange}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default SearchBar;
