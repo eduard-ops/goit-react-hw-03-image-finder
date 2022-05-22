5726b4caa67f290733ebb49e9d3708867379689e
eduard-ops <epustynnik@gmail.com>
2022-05-22 13:53:58 +0300

    ImageGalleryItem.js

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -0,0 +1,16 @@
+export default function ImageGalleryItem({ hits, onClickImg }) {
+  return hits.map(({ id, webformatURL, tags, largeImageURL }) => {
+    return (
+      <li key={id} className="gallery__item">
+        {
+          <img
+            onClick={() => onClickImg(largeImageURL, tags)}
+            className="gallery__item-image"
+            src={webformatURL}
+            alt={tags}
+          />
+        }
+      </li>
+    );
+  });
+}
