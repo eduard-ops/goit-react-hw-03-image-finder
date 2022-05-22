fd559074862942143b8a6464e99e89c0f5fa8bb6
eduard-ops <epustynnik@gmail.com>
2022-05-22 13:54:42 +0300

    Modal\Modal.js

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,36 @@
+import { Component } from 'react';
+
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.onCloseEsc);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.onCloseEsc);
+  }
+
+  onCloseEsc = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  onCloseBackdrop = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    const { bigImage, tags } = this.props;
+    return (
+      <div className="backdrop" onClick={this.onCloseBackdrop}>
+        <div className="modal">
+          <img className="modal-img" src={bigImage} alt={tags} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
