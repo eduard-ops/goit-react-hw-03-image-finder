4f09919b8e1c6014053b3ad02e55f2f8e2a0c202
eduard-ops <epustynnik@gmail.com>
2022-05-23 23:20:12 +0300

    Update galleryApi.js

--- src/services/galleryApi.js
+++ src/services/galleryApi.js
@@ -2,6 +2,9 @@ const axios = require('axios');
 const BAZE_URL = 'https://pixabay.com/api/';
 const API_KEY = '25658613-98e642d2c9015137af6c2464f';
 
+// axios.defaults.baseUrl = BAZE_URL;
+// axios.defaults.headers.common['Authorization'] = API_KEY;
+
 export default class GalleryApiService {
   constructor() {
     this.searchQuery = '';
@@ -9,17 +12,18 @@ export default class GalleryApiService {
     this.per_page = 12;
   }
   async fetchGallery() {
-    const searchParams = new URLSearchParams({
-      q: this.searchQuery,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-      per_page: this.per_page,
-      page: this.page,
+    const url = await axios.get(BAZE_URL, {
+      params: {
+        key: API_KEY,
+        q: this.searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: ' true',
+        per_page: this.per_page,
+        page: this.page,
+      },
     });
-    const url = await axios.get(`${BAZE_URL}?key=${API_KEY}&${searchParams}`);
-    const datafetch = await url;
-    const { data } = datafetch;
+    const { data } = url;
     if (!data.hits.length) {
       throw new Error(data.hits.status);
     }
